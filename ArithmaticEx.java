9165ca6ba0802eb4aa8477251eb10544cba04620
chaitanyapoighal <110621512+chaitanyapoighal@users.noreply.github.com>
2025-06-02 16:56:37 +0530

    Add files via upload

---
+++ ArithmaticEx.java
@@ -0,0 +1,11 @@
+public class ArithmaticEx{
+  public static void main(String args[]){
+    int num1=20;
+    int num2=5;
+    System.out.println("sum="+(num1+num2));
+    System.out.println("Difference="+(num1-num2));
+    System.out.println("Product="+(num1*num2));
+    System.out.println("Division="+(num1/num2));
+    System.out.println("Reminder="+(num1%num2));
+   }
+ }
---
+++ Unary.java
@@ -0,0 +1,15 @@
+//Program using Unary Operator
+public class Unary
+{
+  public static void main(String args[])
+  {
+     int num1=10;
+      //pre increment   
+     int num2=++num1;//num1=11 , num2=11 
+
+      //post increment
+     int num3=num1++;// num3=11, num1=12
+     System.out.println("num1="+num1+" num2="+num2+" num3="+num3);
+     // 
+  }
+}
